473088d70d72662cdb44d660ec8c01c7f39bcfdc
Sergey Nesterov <menestrel.me@gmail.com>
2017-10-22 16:10:25 -0700

    Debug build.

--- cmake/CAPI.cmake
+++ cmake/CAPI.cmake
@@ -10,12 +10,13 @@ set( ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${CAPI_ROOT}/lib/pkgconfig" )
 
 include( ProcessorCount )
 ProcessorCount( NPROC )
+message( "Building in ${NPROC} threads" )
 
 configure_file( ${PROJECT_SOURCE_DIR}/cmake/FIX_WRONG_DLT_CONFIG.cmake.in ${CAPI_DIR}/FIX_WRONG_DLT_CONFIG.cmake )
 configure_file( ${PROJECT_SOURCE_DIR}/cmake/CAPI.cmake.in ${CAPI_DIR}/CMakeLists.txt )
 execute_process( COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" . WORKING_DIRECTORY "${CAPI_DIR}" )
 #execute_process( COMMAND "${CMAKE_COMMAND}" --build . -- -j${NPROC} WORKING_DIRECTORY "${CAPI_DIR}" )
-execute_process( COMMAND "${CMAKE_COMMAND}" --build . WORKING_DIRECTORY "${CAPI_DIR}" )
+execute_process( COMMAND "${CMAKE_COMMAND}" --build . -- -j4 WORKING_DIRECTORY "${CAPI_DIR}" )
 
 find_package( CommonAPI REQUIRED ${CAPI_VERSION} )
 find_package( CommonAPI-DBus REQUIRED ${CAPI_VERSION} )
